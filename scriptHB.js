db0ead4289eb0a1547539e05be8d8998f267a2a3
Daniel Bercea <128615941+didocs@users.noreply.github.com>
2023-08-09 14:23:03 +0300

    Update scriptHB.js

--- scriptHB.js
+++ scriptHB.js
@@ -9,38 +9,38 @@ let randomList = [], winner = "Wiiinner!!:)";
 function assignWinnerButton() {
 	let n = message.length, i;
 	while (n) {
-    i = Math.floor(Math.random() * n);
-    randomList.push(message.splice(i, 1)[0]);
-    --n;
-  }
-  return randomList;
+	i = Math.floor(Math.random() * n);
+	randomList.push(message.splice(i, 1)[0]);
+	--n;
+	}
+	return randomList;
 }
 
 let idBtn;
 
 function winnerLooser(id) {
-  let btn = document.getElementById(id);
-  let index = id.valueOf();
+	let btn = document.getElementById(id);
+	let index = id.valueOf();
 	btn.innerHTML = randomList[index];
 	idBtn = id;
 	flashLoop();
 }
 
 function flashLoop() {
-  let btn = document.getElementById(idBtn);
-  if (btn.innerHTML === winner) {
-    flashWinner();
-  } else {
-    flashLooser();
-  }
+	let btn = document.getElementById(idBtn);
+	if (btn.innerHTML === winner) {
+		flashWinner();
+	} else {
+		flashLooser();
+	}
 }
 
 function flashWinner() {
 	let flashGreen = document.getElementById(idBtn);
-  flashGreen.className = flashGreen.className === "btn btn-outline-secondary"? "btn btn-outline-success" : "btn btn-outline-secondary";
+	flashGreen.className = flashGreen.className === "btn btn-outline-secondary"? "btn btn-outline-success" : "btn btn-outline-secondary";
 }
 
 function flashLooser() {
 	let flashRed = document.getElementById(idBtn);
-  flashRed.className =  flashRed.className === "btn btn-outline-secondary"? "btn btn-outline-danger" : "btn btn-outline-secondary";
+	flashRed.className =  flashRed.className === "btn btn-outline-secondary"? "btn btn-outline-danger" : "btn btn-outline-secondary";
 }
